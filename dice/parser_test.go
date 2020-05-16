4cb6d9ce071032195cc1d5b1d513193256922f56
John Cater <katre@henchmonkey.org>
2020-05-16 17:49:10 -0400

    Expanded tests

--- dice/parser_test.go
+++ dice/parser_test.go
@@ -11,6 +11,20 @@ import (
 func TestParser(t *testing.T) {
 	parser := New(dice.New(1))
 	require.NotNil(t, parser)
+	assert.Equal(t, "2", parser.Roll("1"))
 
-	assert.Equal(t, "2", parser.Roll("tech 1"))
+	parser = New(dice.New(1))
+	assert.Equal(t, "3", parser.Roll("1 push 1"))
+
+	parser = New(dice.New(1))
+	assert.Equal(t, "3.1", parser.Roll("2 push 1"))
+
+	parser = New(dice.New(1))
+	assert.Equal(t, "5", parser.Roll("3 push 2"))
+
+	parser = New(dice.New(1))
+	assert.Equal(t, "2", parser.Roll("2 hurt 1"))
+
+	parser = New(dice.New(1))
+	assert.Equal(t, "2", parser.Roll("1 push 1 hurt 1"))
 }
