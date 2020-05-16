17c1be8712bde3d0ad73d84ac4360f080edf8b17
John Cater <katre@henchmonkey.org>
2020-05-16 17:42:09 -0400

    Implemented basic technoir dice

--- dice/dice.go
+++ dice/dice.go
@@ -1,9 +1,59 @@
 package dice
 
 import (
+	"fmt"
 	"math/rand"
 )
 
-func rollOne() int {
-	return rand.Intn(5) + 1
+type Roller struct {
+	rand *rand.Rand
+}
+
+func New(seed int64) *Roller {
+	s := rand.NewSource(seed)
+	return &Roller{
+		rand: rand.New(s),
+	}
+}
+
+func (r *Roller) rollOne() int {
+	return r.rand.Intn(5) + 1
+}
+
+func (r *Roller) Roll(verb int, push int, hurt int) string {
+	dice := verb + push
+	// Roll the positive dice.
+	results := make([]int, 0, dice)
+	for i := 0; i < dice; i++ {
+		result := r.rollOne()
+		results = append(results, result)
+	}
+
+	// Roll the hurt dice and remove any matches.
+	for i := 0; i < hurt; i++ {
+		remove := r.rollOne()
+		for i, result := range results {
+			if result == remove {
+				results[i] = 0
+			}
+		}
+	}
+
+	// Find the highest result.
+	highest := 0
+	count := 0
+	for _, result := range results {
+		if result > highest {
+			highest = result
+			count = 1
+		} else if result == highest {
+			count++
+		}
+	}
+
+	if count > 1 {
+		return fmt.Sprintf("%d.1", highest)
+	} else {
+		return fmt.Sprintf("%d", highest)
+	}
 }
--- dice/dice_test.go
+++ dice/dice_test.go
@@ -7,9 +7,29 @@ import (
 )
 
 func TestRollOnce(t *testing.T) {
+	roller := New(1)
 	for i := 0; i < 100; i++ {
-		value := rollOne()
+		value := roller.rollOne()
 		assert.Greater(t, value, 0)
 		assert.Less(t, value, 7)
 	}
+}
+
+func TestRoll(t *testing.T) {
+	// A static seed makes the rolls deterministic.
+	// Sequence: 2 3 3 5 2
+	roller := New(1)
+	assert.Equal(t, "2", roller.Roll(1, 0, 0))
+
+	roller = New(1)
+	assert.Equal(t, "3", roller.Roll(1, 1, 0))
+
+	roller = New(1)
+	assert.Equal(t, "3.1", roller.Roll(3, 0, 0))
+
+	roller = New(1)
+	assert.Equal(t, "5", roller.Roll(3, 2, 0))
+
+	roller = New(1)
+	assert.Equal(t, "2", roller.Roll(2, 0, 1))
 }
