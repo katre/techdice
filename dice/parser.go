08f76c5a50e7e7588b801534ae895739abf44934
John Cater <katre@henchmonkey.org>
2020-05-17 10:54:29 -0400

    Added input parsing

--- dice/parser.go
+++ dice/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"strings"
 
 	"aoc/util/lexer"
@@ -17,10 +18,38 @@ func New(roller *dice.Roller) *Parser {
 	}
 }
 
-func (p *Parser) Roll(input string) string {
+func (p *Parser) Roll(input string) (dice.Result, error) {
 	s := lexer.NewScanner(strings.NewReader(input))
-	if s == nil {
-		// panic
+
+	// Expected format: NUM (push NUM)? (hurt NUM)?
+	verb, err := s.ScanNumber("verb")
+	if err != nil {
+		return dice.Result{}, err
 	}
-	return ""
+
+	// scan until end of input
+	var push, hurt int
+	for {
+		token := s.Scan()
+		if token.TokenType == lexer.EOF {
+			break
+		} else if token.TokenType == lexer.IDENT && token.Literal == "push" {
+			value, err := s.ScanNumber("push")
+			if err != nil {
+				return dice.Result{}, err
+			}
+			push += value
+		} else if token.TokenType == lexer.IDENT && token.Literal == "hurt" {
+			value, err := s.ScanNumber("hurt")
+			if err != nil {
+				return dice.Result{}, err
+			}
+			hurt += value
+		} else {
+			// Unknown token.
+			return dice.Result{}, fmt.Errorf("Unknown token %q", token)
+		}
+	}
+
+	return p.roller.Roll(verb, push, hurt), nil
 }
--- dice/parser_test.go
+++ dice/parser_test.go
@@ -9,22 +9,22 @@ import (
 )
 
 func TestParser(t *testing.T) {
-	parser := New(dice.New(1))
-	require.NotNil(t, parser)
-	assert.Equal(t, "2", parser.Roll("1"))
+	check := func(expectedDice []int, expectedScore string, input string) {
+		// A static seed makes the rolls deterministic.
+		// Sequence: 2 3 3 5 2
+		parser := New(dice.New(1))
+		require.NotNil(t, parser)
+		result, err := parser.Roll(input)
+		if assert.Nil(t, err) {
+			assert.Equal(t, expectedDice, result.Dice)
+			assert.Equal(t, expectedScore, result.Score)
+		}
+	}
 
-	parser = New(dice.New(1))
-	assert.Equal(t, "3", parser.Roll("1 push 1"))
-
-	parser = New(dice.New(1))
-	assert.Equal(t, "3.1", parser.Roll("2 push 1"))
-
-	parser = New(dice.New(1))
-	assert.Equal(t, "5", parser.Roll("3 push 2"))
-
-	parser = New(dice.New(1))
-	assert.Equal(t, "2", parser.Roll("2 hurt 1"))
-
-	parser = New(dice.New(1))
-	assert.Equal(t, "2", parser.Roll("1 push 1 hurt 1"))
+	check([]int{2}, "2", "1")
+	check([]int{2, 3}, "3", "1 push 1")
+	check([]int{2, 3, 3}, "3.1", "2 push 1")
+	check([]int{2, 3, 3, 5, 2}, "5", "3 push 2")
+	check([]int{2}, "2", "2 hurt 1")
+	check([]int{2}, "2", "1 push 1 hurt 1")
 }
